b205ef750bbd4092a39d552561dd777cdfd0a621
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2025-11-10 20:30:12 +0100

    Library styles and scriptsd folder structure

--- functions.php
+++ functions.php
@@ -6,14 +6,39 @@
 */
 
 function blockwise_enqueue_styles() {
-    wp_enqueue_style(
+
+    // register style
+    wp_register_style(
         'stylesheet', 
         get_stylesheet_uri(), 
         [], 
         filemtime(get_template_directory().'/style.css')
     );
+
+    // enqueue Style
+    wp_enqueue_style('stylesheet');
+}
+
+
+function blockwise_enqueue_scripts()
+{   
+    // Jquery scripts
+    wp_enqueue_script('jquery');
+
+    // register main theme js
+    wp_register_script(
+        'blockwise-app',
+        get_template_directory_uri().'/assets/js/app.js',
+        ['jquery'],
+        filemtime(get_template_directory().'/assets/js/app.js'),
+        true
+    );
+
+    // register blockwise app main js 
+    wp_enqueue_script('blockwise-app');
 }
 
 add_action( 'wp_enqueue_scripts', 'blockwise_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'blockwise_enqueue_scripts' );
 
 ?>
